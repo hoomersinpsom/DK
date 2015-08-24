7bc5d5f0787ae28d88b8bd9067df5bd3d4b66ae3
Thiago Frias <thiago@fillet.com.br>
2015-08-24 18:03:44 -0300

    Shoot

--- app/app.js
+++ app/app.js
@@ -13,50 +13,60 @@ var mainState = {
 
   },
   create: function(){
-    this.barrelRotate = 0
+
+    game.stage.backgroundColor = '#2d2d2d';
     //  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.startSystem(Phaser.Physics.P2JS);
     // The player and its settings
+
+    game.physics.p2.gravity.y = 450;
+    game.physics.p2.restitution = 0.8;
+    game.physics.p2.friction = 0.1;
+
+    this.player2 = game.add.sprite((game.world.width / 2 ), 100, this.barrel);
 
     this.player = game.add.sprite((game.world.width / 2 ), game.world.height - 100, this.barrel);
     // BALL //
-    this.ball = game.add.sprite(this.player.position.x, this.player.position.y, this.ball);
-    game.physics.enable([this.player,this.ball], Phaser.Physics.ARCADE);
+    this.ball = game.add.sprite(this.player.position.x, this.player.position.y, this.ballShape);
+
+
     this.player.anchor.setTo(0.5, 0.5);
+    this.player2.anchor.setTo(0.5, 0.5);
 
     //  We need to enable physics on the this.player
     this.ball.anchor.setTo(0.5, 0.5);
 
-    game.physics.arcade.gravity.y = 200;
-    this.ball.allowGravity = false
-    this.player.allowGravity = false
-    this.ball.body.collideWorldBounds = true;
-    this.player.body.collideWorldBounds = true;
-    this.ball.body.imovable = true;
-    this.player.body.imovable = true;
 
-    this.player.body.velocity.y = 0
 
     //  We need to enable physics on the this.player
 
     this.tweens.playerRotation = this.add.tween(this.player)
       .to({angle: 359}, 3000, null, true, 0, Infinity);
-    this.tweens.ballRotation = this.add.tween(this.ball)
-      .to({angle: 359}, 3000, null, true, 0, Infinity);
+    this.tweens.player2Rotation = this.add.tween(this.player2)
+      .to({angle: 359}, 2000, null, true, 0, Infinity);
 
     //  this.Player physics properties. Give the little guy a slight bounce
     game.input.onDown.add(this.boom, this);
   },
   boom: function(){
     console.log("boom")
-    this.ball.allowGravity = true
+    game.physics.p2.enable(this.ball);
+    this.tweens.playerRotation.stop()
+
+    this.ball.body.rotation = this.player.rotation + game.math.degToRad(-180);
+
+    var magnitude = 600;
+    var angle = this.ball.body.rotation + Math.PI / 2;
+
+    this.ball.body.velocity.x = magnitude * Math.cos(angle);
+    this.ball.body.velocity.y = magnitude * Math.sin(angle);
     //this.ball.body.velocity.y = -300
   },
   update: function(){
     //sthis.ball.body.velocity.y = 100
   },
   render: function(){
-    game.debug.spriteInfo(this.ball, 32, 32);
+    //game.debug.spriteInfo(this.ball, 32, 32);
   },
   /*******************
   //     SHAPES     //
@@ -64,32 +74,32 @@ var mainState = {
   // formas geometricas dos quadrados e sprites em geral
   shapes:function(){
 
-    this.barrel = game.add.bitmapData(50,70);
+    this.barrel = game.add.bitmapData(30,50);
 
     // draw to the canvas context like normal
     this.barrel.ctx.beginPath();
-    this.barrel.ctx.rect(0,0,50,70);
+    this.barrel.ctx.rect(0,0,30,50);
     this.barrel.ctx.fillStyle = '#991111';
     this.barrel.ctx.fill();
     this.barrel.ctx.beginPath();
-    this.barrel.ctx.rect(0,0,50,4);
+    this.barrel.ctx.rect(0,0,30,4);
     this.barrel.ctx.fillStyle = '#119911';
     this.barrel.ctx.fill();
 
 
-    this.ball = game.add.bitmapData(30,30);
+    this.ballShape = game.add.bitmapData(20,20);
 
     // draw to the canvas context like normal
 
-    this.ball.ctx.beginPath();
-    this.ball.ctx.arc(15, 15, 15, 0, 2 * Math.PI, false);
-    this.ball.ctx.fillStyle = 'green';
-    this.ball.ctx.fill();
-    this.ball.ctx.stroke();
-    this.ball.ctx.beginPath();
-    this.ball.ctx.rect(7,5,3,3);
-    this.ball.ctx.fillStyle = 'tomato';
-    this.ball.ctx.fill();
+    this.ballShape.ctx.beginPath();
+    this.ballShape.ctx.arc(10, 10, 10, 0, 2 * Math.PI, false);
+    this.ballShape.ctx.fillStyle = 'green';
+    this.ballShape.ctx.fill();
+    this.ballShape.ctx.stroke();
+    this.ballShape.ctx.beginPath();
+    this.ballShape.ctx.rect(7,5,3,3);
+    this.ballShape.ctx.fillStyle = 'tomato';
+    this.ballShape.ctx.fill();
 
   }
 }
